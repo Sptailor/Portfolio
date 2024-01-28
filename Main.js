14498772c4cee8f182e62402b1fce9614ab4f7e4
stailor-creator <114362930+stailor-creator@users.noreply.github.com>
2024-01-28 02:33:29 -0600

    Updated initial page/injected Js

---
+++ Main.js
@@ -0,0 +1,10 @@
+
+
+var typingEffect = new Typed(".typer",{
+    strings : ["Computer Science Major ","C++ Programmer","Java Developer","Web Developer","Database Manager"],
+
+    loop : true,
+    typeSpeed : 50, 
+    backSpeed : 80,
+    backDelay : 2000
+ })

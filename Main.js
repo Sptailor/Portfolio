b580ae154c16143f441bcfd2e8eb132bff159f21
stailor-creator <114362930+stailor-creator@users.noreply.github.com>
2024-01-30 01:20:30 -0600

    Updated navbare/responsive navbar

--- Main.js
+++ Main.js
@@ -1,6 +1,12 @@
 
+const togglebtn=document.getElementsByClassName('toggle-button')[0]
 
-var typingEffect = new Typed(".typer",{
+const navlinks=document.getElementsByClassName('nav')[0]
+
+const navbar=document.getElementById("outer-nav");
+
+
+var typingEffect = new Typed(".typer",{//typing effect
     strings : ["Computer Science Major ","C++ Programmer","Java Developer","Web Developer","Database Manager"],
 
     loop : true,
@@ -8,3 +14,18 @@ var typingEffect = new Typed(".typer",{
     backSpeed : 80,
     backDelay : 2000
  })
+togglebtn.addEventListener('click',()=>{//toggle navbar
+    navlinks.classList.toggle('active')
+    togglebtn.classList.toggle('rotate')
+    if(navbar.classList.contains('slide-in')){
+        navbar.classList.remove('slide-in')
+        navbar.classList.add('slide-out')
+    }else if(navbar.classList.contains('slide-out')){
+        navbar.classList.remove('slide-out')
+        navbar.classList.add('slide-in')
+    }
+    
+    
+
+ 
+})
